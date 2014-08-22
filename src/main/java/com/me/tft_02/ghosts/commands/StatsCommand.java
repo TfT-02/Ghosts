0980f50a7427a9dcb01fee442d5afdf5de386833
TfT_02 <tft_02@hotmail.com>
2014-08-22 14:20:05 -0700

    Prettier stats display

--- src/main/java/com/me/tft_02/ghosts/commands/StatsCommand.java
+++ src/main/java/com/me/tft_02/ghosts/commands/StatsCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 
 import com.me.tft_02.ghosts.datatypes.StatsType;
 import com.me.tft_02.ghosts.datatypes.player.GhostPlayer;
+import com.me.tft_02.ghosts.items.ResurrectionScroll.Tier;
 import com.me.tft_02.ghosts.locale.LocaleLoader;
 import com.me.tft_02.ghosts.util.CommandUtils;
 import com.me.tft_02.ghosts.util.player.UserManager;
@@ -30,9 +31,15 @@ public class StatsCommand implements CommandExecutor {
 //        }
         GhostPlayer ghostPlayer = UserManager.getPlayer(player);
 
-        for (StatsType statsType : StatsType.values()) {
-            player.sendMessage(LocaleLoader.getString("Commands.Stats.DisplayStat", statsType.toString(), ghostPlayer.getStats(statsType)));
-        }
+        String indent = "  ";
+        player.sendMessage(LocaleLoader.getString("Commands.Stats.Header"));
+        player.sendMessage(indent + LocaleLoader.getString("Commands.Stats.Deaths" , ghostPlayer.getStats(StatsType.DEATHS)));
+        player.sendMessage(indent + LocaleLoader.getString("Commands.Stats.TombsFound" , ghostPlayer.getStats(StatsType.FIND_TOMB)));
+        player.sendMessage(indent + LocaleLoader.getString("Commands.Stats.GivenUp" , ghostPlayer.getStats(StatsType.GIVEN_UP)));
+        player.sendMessage(LocaleLoader.getString("Commands.Stats.ItemsUsed"));
+        player.sendMessage(indent + LocaleLoader.getString("Commands.Stats.RessScroll", Tier.ONE.toNumerical(), ghostPlayer.getStats(StatsType.RESURRECTION_SCROLLS_USED_T1), ghostPlayer.getStats(StatsType.RESURRECTION_SCROLLS_USED_OTHERS_T1), ghostPlayer.getStats(StatsType.RESURRECTION_SCROLLS_RECEIVED_T1)));
+        player.sendMessage(indent + LocaleLoader.getString("Commands.Stats.RessScroll", Tier.TWO.toNumerical(), ghostPlayer.getStats(StatsType.RESURRECTION_SCROLLS_USED_T2), ghostPlayer.getStats(StatsType.RESURRECTION_SCROLLS_USED_OTHERS_T2), ghostPlayer.getStats(StatsType.RESURRECTION_SCROLLS_RECEIVED_T2)));
+        player.sendMessage(indent + LocaleLoader.getString("Commands.Stats.RessScroll", Tier.THREE.toNumerical(), ghostPlayer.getStats(StatsType.RESURRECTION_SCROLLS_USED_T3), ghostPlayer.getStats(StatsType.RESURRECTION_SCROLLS_USED_OTHERS_T3), ghostPlayer.getStats(StatsType.RESURRECTION_SCROLLS_RECEIVED_T3)));
 
         return true;
     }
